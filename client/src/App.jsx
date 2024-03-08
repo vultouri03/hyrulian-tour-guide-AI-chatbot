08d935e83a5bf5468c775936def57925f0168165
vultouri03 <youridegier@gmail.com>
2024-03-08 16:34:27 +0100

    build react frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -66,7 +66,7 @@ function App() {
 
         try {
             setDone("")
-            const response = await fetch("http://localhost:8000/chat", options)
+            const response = await fetch("https://hyrulian-tour-guide-ai-chatbot-1.onrender.com/chat", options)
 
             async function streamToString(body) {
 
@@ -130,7 +130,7 @@ const historyList = history.map((items, i) => {
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-2/3 px-3">
               <label htmlFor="prompt" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">you</label>
-              <input type="text" id="prompt" name="prompt" placeholder="What is your question" value={speech}
+              <input type="text" id="prompt" name="prompt" placeholder="What is your question"  defaultValue={speech}
                      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"/>
             </div>
             <div className="w-full md:w-1/3 px-3">
